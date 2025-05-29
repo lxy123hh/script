e82f6e13911e6368b4df608edf6ff98b630955db
lxy123hh <1677837750@qq.com>
2025-05-29 10:39:09 +0800

    feat:finish font

--- 画折线图+柱状图.py
+++ 画折线图+柱状图.py
@@ -5,4 +5,8 @@ import matplotlib.font_manager as fm
 #设置中文字体
 try:
     font_path = 'C:\Windows\Fonts\simsun.ttc'
-    font_
+    font_prop = fm.FontProperties(fname=font_path, size=12)
+except:
+    font_prop = None
+
+#数据准备
